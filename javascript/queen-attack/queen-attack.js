85e8c693df127320e76bd377dbf525e793dd14d1
GRM <grmorgado@gmail.com>
2019-04-26 12:35:53 +0100

    queenAttack test2 passed

--- javascript/queen-attack/queen-attack.js
+++ javascript/queen-attack/queen-attack.js
@@ -1,7 +1,20 @@
 export class QueenAttack {
-  constructor(white, black) {
-    !white ? (this.white = [0, 3]) : (this.white = white);
-    !black ? (this.black = [7, 3]) : (this.black = black);
+  //   _white = [0, 3];
+  //   _black = [7, 3];
+  constructor(pos) {
+    // console.log("this", this);
+    // this.white = [0, 3];
+    // this.black = [7, 3];
+    // {
+    //   (white = [0, 3]), (black = [7, 3]);
+    // }
+    if (!pos) {
+      this.white = [0, 3];
+      this.black = [7, 3];
+    } else {
+      !pos.white ? (this.white = [0, 3]) : (this.white = pos.white);
+      !pos.black ? (this.black = [7, 3]) : (this.black = pos.black);
+    }
   }
 }
 //  (white, black) => {
