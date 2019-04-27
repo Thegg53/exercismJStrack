5d4b731eb152e12da9957f1c0e24f4e867a89de6
guilherme <grmorgado@gmail.com>
2019-04-27 22:51:28 +0100

    revamp TwoFer + start Resistor Color

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,7 +1,1 @@
-export const twoFer = name => {
-  if (name) {
-    return `One for ${name}, one for me.`;
-  } else {
-    return `One for you, one for me.`;
-  }
-};
+export const twoFer = (name = "you") => `One for ${name}, one for me.`;
