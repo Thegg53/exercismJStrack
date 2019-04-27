0ee1a2d84e5dc75543a400285ef1857b0ba9c5e0
guilherme <grmorgado@gmail.com>
2019-04-28 00:14:24 +0100

    runLengthEncoding test11 passed

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -19,7 +19,6 @@ function createArray(a) {
 }
 
 export const encode = sInput => {
-  debugger;
   let aInput = sInput.split("");
 
   let a = createArray(aInput);
@@ -29,26 +28,29 @@ export const encode = sInput => {
 };
 
 export const decode = sInput => {
-  let regEx = /\d*\w|\s*/g;
+  let regEx = /\d*\s|\d*\w/g;
   let groups = sInput.match(regEx); //split when a new letter or whitespace is found
 
   let sOutput = "";
-  for (let i = 0; i < groups.length; i++) {
-    let s = "";
-    let j = 0;
-    if (groups[i].length > 1) {
-      //if the group has length more than one, there is/are numbers and then the character
+  if (groups) {
+    for (let i = 0; i < groups.length; i++) {
+      let s = "";
 
-      let n = parseInt(groups[i].substring(0, groups[i].length - 1));
-      while (j < n) {
-        s = s + groups[i].substring(groups[i].length - 1);
-        j++;
+      if (groups[i].length > 1) {
+        //if the group has length more than one, there is/are numbers and then the character
+        let j = 0;
+        let n = parseInt(groups[i].substring(0, groups[i].length - 1));
+        while (j < n) {
+          debugger;
+          s = s + groups[i].substring(groups[i].length - 1);
+          j++;
+        }
+      } else {
+        //if the length of the group is one, then the group is just the character we want
+        s = groups[i];
       }
-    } else {
-      //if the length of the group is one, then the group is just the character we want
-      s = groups[i];
+      sOutput = sOutput + s;
     }
-    sOutput = sOutput + s;
   }
   console.log("sOutput", sOutput);
   return sOutput;
