804b109d6ce5566955604687933c9e04698fc6b2
GRM <grmorgado@gmail.com>
2019-05-03 17:48:01 +0100

    reverseString t2 passed

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,5 +1,10 @@
 export { reverseString };
 
 const reverseString = sInput => {
-  return sInput;
+  let aInput = sInput.split("");
+  let sOutput = "";
+  for (let i = aInput.length - 1; i >= 0; i--) {
+    sOutput = sOutput + aInput[i];
+  }
+  return sOutput;
 };
--- javascript/reverse-string/reverse-string.spec.js
+++ javascript/reverse-string/reverse-string.spec.js
@@ -1,33 +1,33 @@
-import { reverseString } from './reverse-string';
+import { reverseString } from "./reverse-string";
 
-describe('ReverseString', () => {
-  test('empty string', () => {
-    const expected = '';
-    const actual = reverseString('');
+describe("ReverseString", () => {
+  test("empty string", () => {
+    const expected = "";
+    const actual = reverseString("");
     expect(actual).toEqual(expected);
   });
 
-  xtest('a word', () => {
-    const expected = 'tobor';
-    const actual = reverseString('robot');
+  test("a word", () => {
+    const expected = "tobor";
+    const actual = reverseString("robot");
     expect(actual).toEqual(expected);
   });
 
-  xtest('a capitalized word', () => {
-    const expected = 'nemaR';
-    const actual = reverseString('Ramen');
+  xtest("a capitalized word", () => {
+    const expected = "nemaR";
+    const actual = reverseString("Ramen");
     expect(actual).toEqual(expected);
   });
 
-  xtest('a sentence with punctuation', () => {
-    const expected = '!yrgnuh ma I';
-    const actual = reverseString('I am hungry!');
+  xtest("a sentence with punctuation", () => {
+    const expected = "!yrgnuh ma I";
+    const actual = reverseString("I am hungry!");
     expect(actual).toEqual(expected);
   });
 
-  xtest('a palindrome', () => {
-    const expected = 'racecar';
-    const actual = reverseString('racecar');
+  xtest("a palindrome", () => {
+    const expected = "racecar";
+    const actual = reverseString("racecar");
     expect(actual).toEqual(expected);
   });
 });
