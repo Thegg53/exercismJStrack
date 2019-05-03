31e4d0bdddf1af4b1d57e4631b1db91a0953ba01
GRM <grmorgado@gmail.com>
2019-05-03 17:59:02 +0100

    cleaner version of reverseString

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,10 +1,3 @@
 export { reverseString };
 
-const reverseString = sInput => {
-  let aInput = sInput.split("");
-  let sOutput = "";
-  for (let i = aInput.length - 1; i >= 0; i--) {
-    sOutput = sOutput + aInput[i];
-  }
-  return sOutput;
-};
+const reverseString = sInput => sInput.split("").reverse().join("");
