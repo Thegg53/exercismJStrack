7a51a14138ae93b474805358d8a4d9246781c4a4
GRM <grmorgado@gmail.com>
2019-05-03 18:59:35 +0100

    triangle t7,8,9,10 passed

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -8,7 +8,8 @@ export class Triangle {
     kind() {
         if (this.a == this.b || this.a == this.c) {
             return this.b == this.c ? 'equilateral' : 'isosceles'
-        } else if (this.b == this.c) return 'isosceles'
+        } else if (this.b == this.c) { return 'isosceles' }
+        else return 'scalene'
         // return 'equilateral'
     }
 }
