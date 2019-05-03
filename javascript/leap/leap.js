fd268ccbfe4fdeab6ea15013b71fc045e5e1158d
GRM <grmorgado@gmail.com>
2019-05-03 18:14:13 +0100

    leap one-line

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,7 +1,1 @@
-export const isLeap = (iYear) => {
-    if (iYear % 4 == 0 && (iYear % 100 != 0 || iYear % 400 === 0)) {
-        return true
-    } else {
-        return false
-    }
-}
+export const isLeap = (iYear) => iYear % 4 == 0 && (iYear % 100 != 0 || iYear % 400 === 0)
