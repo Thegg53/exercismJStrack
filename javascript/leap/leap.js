3a303c9dbe06e5e2fefee60fea24db9ddfa606cb
GRM <grmorgado@gmail.com>
2019-05-03 18:10:33 +0100

    leap minor cleanup

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,6 +1,4 @@
-export { isLeap };
-
-const isLeap = (iYear) => {
+export const isLeap = (iYear) => {
     if (iYear % 4 == 0 && (iYear % 100 != 0 || iYear % 400 === 0)) {
         return true
     } else {
