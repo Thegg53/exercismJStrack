133ca82e051137a3360614d15eb9e5a4c7bb44bd
GRM <grmorgado@gmail.com>
2019-05-03 18:24:11 +0100

    collatzConjecture  all tests passed

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,5 +1,8 @@
 export const steps = (iNumber) => {
     let counter = 0;
+    if (iNumber <= 0) {
+        throw new Error('Only positive numbers are allowed')
+    }
     while (iNumber !== 1) {
         iNumber % 2 === 0 ? iNumber = iNumber / 2 : iNumber = 3 * iNumber + 1;
         counter++;
