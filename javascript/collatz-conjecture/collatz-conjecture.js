39c9c1e5a5a85d3cafd01d1425cc9121dff68c9a
GRM <grmorgado@gmail.com>
2019-05-03 18:21:36 +0100

    collatzConjecture t2 passed

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,1 +1,8 @@
-export const steps = (iYear) => 0;
+export const steps = (iNumber) => {
+    let counter = 0;
+    while (iNumber !== 1) {
+        iNumber % 2 === 0 ? iNumber = iNumber / 2 : iNumber = 3 * iNumber + 1;
+        counter++;
+    }
+    return counter;
+};
