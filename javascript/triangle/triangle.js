63086b81a3985f2855aa177788686cc62c68f79f
GRM <grmorgado@gmail.com>
2019-05-04 18:25:43 +0100

    triangle t11 passed

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -6,10 +6,12 @@ export class Triangle {
     }
 
     kind() {
+        if (this.a <= 0 && this.b <= 0 && this.c <= 0) {
+            throw '';
+        }
         if (this.a == this.b || this.a == this.c) {
             return this.b == this.c ? 'equilateral' : 'isosceles'
         } else if (this.b == this.c) { return 'isosceles' }
         else return 'scalene'
-        // return 'equilateral'
     }
 }
