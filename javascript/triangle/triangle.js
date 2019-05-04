e4afc63144a29d446f4d8176e6879e4caf653981
GRM <grmorgado@gmail.com>
2019-05-04 18:34:56 +0100

    triangles cleanup

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -6,12 +6,9 @@ export class Triangle {
     }
 
     kind() {
-        if (this.a <= 0 || this.b <= 0 || this.c <= 0 || (this.a > this.b + this.c || this.b > this.a + this.c || this.c > this.b + this.a)) {
-            throw '';
-        }
-        if (this.a == this.b || this.a == this.c) {
-            return this.b == this.c ? 'equilateral' : 'isosceles'
-        } else if (this.b == this.c) { return 'isosceles' }
+        if (this.a <= 0 || this.b <= 0 || this.c <= 0 || (this.a > this.b + this.c || this.b > this.a + this.c || this.c > this.b + this.a)) throw new Error();
+        if (this.a == this.b || this.a == this.c) return this.b == this.c ? 'equilateral' : 'isosceles';
+        else if (this.b == this.c) { return 'isosceles' }
         else return 'scalene'
     }
 }
