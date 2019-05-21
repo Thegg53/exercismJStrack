572e2940595b9586822d69512e0fbff14eed1c81
GRM <grmorgado@gmail.com>
2019-05-21 19:28:02 +0100

    JS romanNumerals t6

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,1 +1,23 @@
-export const toRoman = () => 'I';
+export const toRoman = (inputN) => {
+    let s = '';
+
+    let thousands = Math.floor(inputN / 1000);
+    let hundreds = inputN % 1000;
+    let decs = inputN % 100;
+
+
+    let sing = inputN % 10;
+    if (sing != 0) {
+        if (sing >= 1 && sing <= 3) {
+            s = s + 'I'.repeat(sing);
+        } else if (sing == 4) {
+            s = s + 'IV';
+        }
+        else if (sing >= 5 && sing <= 8) {
+            s = s + 'V' + 'I'.repeat(sing - 5);
+        } else {
+            s = s + 'IX'
+        }
+    }
+    return s;
+} 
